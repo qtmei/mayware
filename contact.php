b9a76c17b38f40baac86d398483219a08e0061ff
Mei <92554133+qtmei@users.noreply.github.com>
2025-02-06 08:47:42 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -51,7 +51,7 @@
 		<meta name="robots" content="index, follow"/>
 		<meta name="language" content="English"/>
 
-		<link rel="icon" href="favicon.ico"/>
+		<link rel="icon" type="image/x-icon" href="favicon.ico"/>
 		<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32"/>
 		<link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196"/>
 		<link rel="icon" type="image/png" href="favicon-1080x1080.png" sizes="1080x1080"/>
